2f4891357f0935af77535fbfab2f69b7aed74eae
anda522 <wyq522208@qq.com>
2025-04-28 11:15:19 +0800

    feat: Add WarmupMultistepLR

---
+++ Tools/optimizer.py
@@ -0,0 +1,49 @@
+import torch
+from bisect import bisect_right
+
+# 学习率调度器 MultiStepLR + warmup
+# 训练刚开始时用一个「warmup」阶段（即前几百步让学习率慢慢升上来），然后在设定的 milestones（训练轮次，比如第 30、60、90 个 epoch）时，
+# 把学习率乘以一个 gamma 值（通常小于 1，比如 0.1），以便让学习率逐步下降
+class WarmupMultiStepLR(torch.optim.lr_scheduler._LRScheduler):
+    def __init__(
+        self,
+        optimizer,
+        milestones,
+        gamma=0.1,
+        warmup_factor=1.0 / 3,
+        warmup_iters=500,
+        warmup_method="linear",
+        last_epoch=-1,
+    ):
+        if not list(milestones) == sorted(milestones):
+            raise ValueError(
+                "Milestones should be a list of" " increasing integers. Got {}",
+                milestones,
+            )
+
+        if warmup_method not in ("constant", "linear"):
+            raise ValueError(
+                "Only 'constant' or 'linear' warmup_method accepted"
+                "got {}".format(warmup_method)
+            )
+        self.milestones = milestones
+        self.gamma = gamma
+        self.warmup_factor = warmup_factor
+        self.warmup_iters = warmup_iters
+        self.warmup_method = warmup_method
+        super(WarmupMultiStepLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        warmup_factor = 1
+        if self.last_epoch < self.warmup_iters:
+            if self.warmup_method == "constant":
+                warmup_factor = self.warmup_factor
+            elif self.warmup_method == "linear":
+                alpha = float(self.last_epoch) / self.warmup_iters
+                warmup_factor = self.warmup_factor * (1 - alpha) + alpha
+        return [
+            base_lr
+            * warmup_factor
+            * self.gamma ** bisect_right(self.milestones, self.last_epoch)
+            for base_lr in self.base_lrs
+        ]
